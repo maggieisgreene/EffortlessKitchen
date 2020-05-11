b28865e4dd653fd65a6f0e7021af1a83271a2215
Maggie Greene <maggie.greene96@gmail.com>
2020-05-11 10:44:49 -0500

    Added functionality to assign Chefs their Menu

--- EffortlessKitchen/Controllers/ChefsController.cs
+++ EffortlessKitchen/Controllers/ChefsController.cs
@@ -4,10 +4,12 @@ using System.Linq;
 using System.Threading.Tasks;
 using EffortlessKitchen.Data;
 using EffortlessKitchen.Models;
+using EffortlessKitchen.Models.ViewModels;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
 using Microsoft.EntityFrameworkCore;
 
 namespace EffortlessKitchen.Controllers
@@ -62,16 +64,34 @@ namespace EffortlessKitchen.Controllers
 
         public async Task<ActionResult> Edit(int id)
         {
-            var chef = await _context.Chef
-                .Where(c => c.ChefId == id)
-                .FirstOrDefaultAsync();
+            var vm = new ChefFormViewModel();
 
-            return View(chef);
+            var chef = await _context.Chef
+                .Include(c => c.ChefMenus)
+                .FirstOrDefaultAsync(c => c.ChefId == id);
+
+            var menuOptions = await _context.MenuOption
+                .Select(mo => new SelectListItem()
+                {
+                    Text = mo.Name,
+                    Value = mo.MenuOptionId.ToString()
+                })
+                .ToListAsync();
+
+            vm.FirstName = chef.FirstName;
+            vm.LastName = chef.LastName;
+            vm.Description = chef.Description;
+            vm.Specialties = chef.Specialties;
+            vm.Price = chef.Price;
+            vm.MenuOptions = menuOptions;
+            vm.SelectedMenuOptionIds = chef.ChefMenus.Select(cm => cm.MenuOptionId).ToList();
+
+            return View(vm);
         }
 
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public async Task<ActionResult> Edit(int id, Chef chef)
+        public async Task<ActionResult> Edit(int id, ChefFormViewModel chef)
         {
             try
             {
@@ -84,6 +104,12 @@ namespace EffortlessKitchen.Controllers
                     Specialties = chef.Specialties,
                     Price = chef.Price
                 };
+
+                uchef.ChefMenus = chef.SelectedMenuOptionIds.Select(menuId => new ChefMenu()
+                {
+                    ChefId = chef.ChefId,
+                    MenuOptionId = menuId
+                }).ToList();
 
                 _context.Chef.Update(uchef);
                 await _context.SaveChangesAsync();
